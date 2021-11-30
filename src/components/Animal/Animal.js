cd25b28d059a0818acfc58e4125eea0f6c12f4a6
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-11-29 16:24:01 -0800

    animal farm first go

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,5 +1,13 @@
 import React from 'react'
 
-export default function Animal() {
-  return <></>
+export default function Animal(props) {
+  return (
+    <>
+      <img height="100" src={`${process.env.PUBLIC_URL}/animals/${props.type}.svg`} />
+      <div className="animal">
+        <p>{props.name}</p>
+        <p>{props.says}</p>
+      </div>
+    </>
+  )
 }
